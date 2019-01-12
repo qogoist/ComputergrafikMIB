568d671dd3e73b5475ca3406862fc55efda49e9c
qogoist <jonas.haller.jh@gmail.com>
2019-01-12 15:31:04 +0100

    Nach Aufgabe 11

--- Tut11_AssetsPicking/AssetsPicking.cs
+++ Tut11_AssetsPicking/AssetsPicking.cs
@@ -20,6 +20,15 @@ namespace Fusee.Tutorial.Core
         private SceneRenderer _sceneRenderer;
         private TransformComponent _baseTransform;
 
+        private ScenePicker _scenePicker;
+        private PickResult _currentPick;
+        private float3 _oldColor;
+        private TransformComponent _turretTransform;
+        private TransformComponent _armTransform;
+        private TransformComponent _bodyTransform;
+        private float _camAngle = 0;
+        private float _speed = 0;
+
         SceneContainer CreateScene()
         {
             // Initialize transform components that need to be changed inside "RenderAFrame"
@@ -63,27 +72,91 @@ namespace Fusee.Tutorial.Core
             // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
             RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
 
-            _scene = CreateScene();
+            _scene = AssetStorage.Get<SceneContainer>("CubeCar.fus");
+
+            _turretTransform = _scene.Children.FindNodes(node => node.Name == "Turret")?.FirstOrDefault()?.GetTransform();
+            _armTransform = _scene.Children.FindNodes(node => node.Name == "Arm")?.FirstOrDefault()?.GetTransform();
+            _bodyTransform = _scene.Children.FindNodes(node => node.Name == "Body")?.FirstOrDefault()?.GetTransform();
 
             // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRenderer(_scene);
+            _scenePicker = new ScenePicker(_scene);
         }
 
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
-
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
-            // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
+            //Setup the Camera
+            if (Mouse.LeftButton)
+            {
+                _speed = Mouse.Velocity.x * 0.000025f;
+                _camAngle -= _speed;
+            }
+            else
+            {
+                _camAngle -= _speed;
+                _speed = _speed * 0.9f;
+            }
+
+            RC.View = float4x4.CreateTranslation(0, -10, 50) * float4x4.CreateRotationY(_camAngle);
+
+            if (Mouse.LeftButton)
+            {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+                _scenePicker.View = RC.View;
+                _scenePicker.Projection = RC.Projection;
+
+                List<PickResult> pickResults = _scenePicker.Pick(pickPosClip).ToList();
+                PickResult newPick = null;
+                if (pickResults.Count > 0)
+                {
+                    pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
+                    newPick = pickResults[0];
+                }
+
+                if (newPick?.Node != _currentPick?.Node)
+                {
+                    if (_currentPick != null)
+                    {
+                        ShaderEffectComponent shaderEffectComponent = _currentPick.Node.GetComponent<ShaderEffectComponent>();
+                        shaderEffectComponent.Effect.SetEffectParam("DiffuseColor", _oldColor);
+                    }
+                    if (newPick != null)
+                    {
+                        ShaderEffectComponent shaderEffectComponent = newPick.Node.GetComponent<ShaderEffectComponent>();
+                        _oldColor = (float3)shaderEffectComponent.Effect.GetEffectParam("DiffuseColor");
+                        shaderEffectComponent.Effect.SetEffectParam("DiffuseColor", new float3(1, 0.4f, 0.4f));
+                    }
+                    _currentPick = newPick;
+                    //Diagnostics.Log(_currentPick.Node.Name);
+                }
+            }
+
+            if (_currentPick != null)
+            {
+                switch (_currentPick.Node.Name)
+                {
+                    case "Arm":
+                        _armTransform.Rotation.x = _armTransform.Rotation.x + Keyboard.WSAxis * Time.DeltaTime;
+                        break;
+                    case "Body":
+                        _bodyTransform.Rotation.y = _bodyTransform.Rotation.y + Keyboard.ADAxis * Time.DeltaTime;
+                        break;
+                    case "Turret":
+                        _turretTransform.Rotation.y = _turretTransform.Rotation.y + Keyboard.ADAxis * Time.DeltaTime;
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
 
-            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
             Present();
         }
 
